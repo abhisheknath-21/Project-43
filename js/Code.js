4b099cfad762c1e6e9d64b914985aff4811a95f3
Abhishek Nath <77669745+abhisheknath-21@users.noreply.github.com>
2021-07-10 06:05:15 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    push();
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70)
+    pop();
+    
+    text("C U T N I F O N", 700, 150);
+    push();
+    fill("lightblue");
+    text("Hint : Perfoms a specific task !!", 700, 170);
+    pop();
+
+    text("A T A D A S B A", 100, 280);
+    push();
+    fill("lightblue");
+    text("Hint : Stores all information !!", 100, 300);
+    pop();
+
+    
+}
